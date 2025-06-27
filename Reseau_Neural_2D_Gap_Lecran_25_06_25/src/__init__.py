aa7842db7448068f3856131e9b21728e34f08c77
Oussama-Guelfaa <guelfaao@gmail.com>
2025-06-27 14:45:29 +0200

    🐍 Add Python package initialization
    - Add __init__.py for proper Python package structure
    - Enable module imports and package organization

---
+++ Reseau_Neural_2D_Gap_Lecran_25_06_25/src/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Package src pour Réseau Neural 2D Gap + L_écran
+Auteur: Oussama GUELFAA
+Date: 25/06/2025
+"""
+
+__version__ = "1.0.0"
+__author__ = "Oussama GUELFAA"
+__date__ = "25/06/2025"
